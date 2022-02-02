e846815070de3fa741cf86603f4a6c51afea2c6d
Roman <romndev@gmail.com>
2022-02-02 13:48:30 -0300

    exercises 3.01-3.08

---
+++ part3-3.10/index.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+morgan.token('data', (req) => {
+  return req.method === 'POST'
+    ? JSON.stringify(req.body)
+    : null
+})
+
+app.use(express.json())
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.data(req, res)
+  ].join(' ')
+}))
+
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+        error: 'name or number missing' 
+
+        })
+    }
+    if(persons.some((person)=> body.name.toLowerCase() == person.name.toLowerCase())){
+        return response.status(400).json({
+            error:'name must be unique'
+    })}
+
+    const id = Math.round(Math.random() * 100000)
+    const person = {
+        id: id,
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    response.json(persons)
+  })
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    date = (new Date()).toUTCString()
+    console.log({date});
+    response.send(`Phonebook has info for ${persons.length} people ${date}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+    response.json(person)
+    } else {
+    response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+  })
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
