9269f11ec7c22b7ef02facd8ed7dd4c33c78efec
Roman <romndev@gmail.com>
2022-02-03 21:52:51 -0300

    minor lint change

--- part2/phonebook/src/services/personService.js
+++ part2/phonebook/src/services/personService.js
@@ -21,5 +21,4 @@ const getAll = () => {
     return request.then(response => response.data)
   }
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default { getAll, create, deletePerson, update}
