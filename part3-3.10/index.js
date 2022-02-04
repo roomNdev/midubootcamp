0dc5e0c436210c81c3cbc9f0d724542adddd5874
Roman <romndev@gmail.com>
2022-02-03 22:43:40 -0300

    env var

--- part3-3.10/index.js
+++ part3-3.10/index.js
@@ -95,7 +95,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
   })
 
-const PORT = 3001
+const PORT = procces.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
